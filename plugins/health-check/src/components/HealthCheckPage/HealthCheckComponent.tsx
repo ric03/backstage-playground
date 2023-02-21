9bc01aa06fbf88e8aeada85fa1509378d83333b5
Tom Schneider <ric0.github@gmail.com>
2023-02-22 00:40:03 +0100

    feat(health-check): add grid layout, add proper history bars

--- plugins/health-check/src/components/HealthCheckPage/HealthCheckComponent.tsx
+++ plugins/health-check/src/components/HealthCheckPage/HealthCheckComponent.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { InfoCard, Progress } from '@backstage/core-components';
-import { Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { EntityRefLink } from '@backstage/plugin-catalog-react';
-import { GetAllResponseEntityInfo } from '@internal/plugin-health-check-common';
+import {
+  GetAllResponseEntityInfo,
+  GetAllResponseHistory,
+} from '@internal/plugin-health-check-common';
 import { useApi } from '@backstage/core-plugin-api';
 import { healthCheckApiRef } from '../../api';
 import { useAsync } from 'react-use';
 import Alert from '@material-ui/lab/Alert';
 import { stringifyEntityRef } from '@backstage/catalog-model';
+
+const greenColor = '#3BA55C';
+const redColor = '#ed4245';
 
 export function HealthCheckOverview() {
   const healthCheckApi = useApi(healthCheckApiRef);
@@ -23,9 +29,13 @@ export function HealthCheckOverview() {
     return <Alert severity="error">{error.message}</Alert>;
   }
 
-  return getAllResponse?.entities.map(data => (
-    <HealthCheckCard key={stringifyEntityRef(data.entityRef)} data={data} />
-  ));
+  return (
+    <React.Fragment>
+      {getAllResponse?.entities.map(data => (
+        <HealthCheckCard key={stringifyEntityRef(data.entityRef)} data={data} />
+      ))}
+    </React.Fragment>
+  );
 }
 
 interface HealthCheckCardOptions {
@@ -35,50 +45,55 @@ interface HealthCheckCardOptions {
 function HealthCheckCard({ data }: HealthCheckCardOptions) {
   return (
     <InfoCard>
-      <EntityRefLink entityRef={data.entityRef} title={data.entityRef.name} />
-      <Typography>{data.status.isHealthy ? 'Up' : 'Down'}</Typography>
-      <br />
-      <Typography>{JSON.stringify(data)}</Typography>
+      <Grid container>
+        <Grid item xs={4}>
+          <EntityRefLink
+            entityRef={data.entityRef}
+            title={data.entityRef.name}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container justifyContent="flex-end">
+            <HistoryBars history={data.history} />
+          </Grid>
+        </Grid>
+        <Grid item xs={2}>
+          {data.status.isHealthy ? (
+            <Typography style={{ color: greenColor }}> Up </Typography>
+          ) : (
+            <Typography style={{ color: redColor }}>Down</Typography>
+          )}
+        </Grid>
+      </Grid>
     </InfoCard>
   );
 }
 
-// export function HealthCheckGrid({
-//   healthCheckResponse,
-// }: HealthCheckGridOptions) {
-//   return (
-//     <Grid container spacing={3} direction="column">
-//       <Grid item>
-//         <GitHubStatusComponent />
-//       </Grid>
-//       {healthCheckResponse &&
-//         healthCheckResponse.entities.map((item, index) => (
-//           <Grid item key={index}>
-//             <HealthCheckComponent item={item} />{' '}
-//           </Grid>
-//         ))}
-//     </Grid>
-//   );
-// }
-//
-// interface HealthCheckComponentOptions {
-//   item: GetAllResponseEntityInfo;
-// }
-//
-// export function HealthCheckComponent({ item }: HealthCheckComponentOptions) {
-//   return (
-//     <InfoCard>
-//       <Grid container spacing={3} alignItems="stretch">
-//         <Grid item md={8}>
-//           <EntityRefLink
-//             entityRef={item.entityRef}
-//             title={item.entityRef.name}
-//           />
-//         </Grid>
-//         <Grid item md={4}>
-//           <Typography>{item.status.isHealthy ? 'Up' : 'Down'}</Typography>
-//         </Grid>
-//       </Grid>
-//     </InfoCard>
-//   );
-// }
+interface HistoryBarsOptions {
+  history: GetAllResponseHistory[];
+}
+
+function HistoryBars({ history }: HistoryBarsOptions) {
+  const bars = history.map((h, index) => (
+    <HistorySingleBar key={index} historyItem={h} />
+  ));
+  return <React.Fragment>{bars}</React.Fragment>;
+}
+
+interface HistorySingleBarOptions {
+  historyItem: GetAllResponseHistory;
+}
+
+function HistorySingleBar({ historyItem }: HistorySingleBarOptions) {
+  return (
+    <div
+      style={{
+        display: 'inline-flex',
+        backgroundColor: historyItem.isHealthy ? greenColor : redColor,
+        width: '0.25rem',
+        height: '1.75rem',
+        marginRight: '0.25rem',
+      }}
+    />
+  );
+}
--- plugins/health-check/src/components/HealthCheckPage/HealthCheckPage.tsx
+++ plugins/health-check/src/components/HealthCheckPage/HealthCheckPage.tsx
@@ -13,12 +13,15 @@ import { HealthCheckOverview } from './HealthCheckComponent';
 export const HealthCheckPage = () => {
   return (
     <Page themeId="tool">
-      <Header title="Welcome to health-check!" subtitle="Optional subtitle">
+      <Header
+        title="Welcome to health-check!"
+        subtitle="Here you can see the uptime of our services"
+      >
         <HeaderLabel label="Owner" value="Team X" />
         <HeaderLabel label="Lifecycle" value="Alpha" />
       </Header>
       <Content>
-        <ContentHeader title="Plugin title">
+        <ContentHeader title="List of health checks">
           <SupportButton>A description of your plugin goes here.</SupportButton>
         </ContentHeader>
         <GitHubStatusCard />
