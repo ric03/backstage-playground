84d31c3595e4f73254b10db2743c833a8450d144
Tom Schneider <ric0.github@gmail.com>
2023-02-28 23:46:02 +0100

    fix: missing parameter in router.test.ts

--- plugins/health-check-backend/src/service/router.test.ts
+++ plugins/health-check-backend/src/service/router.test.ts
@@ -1,9 +1,27 @@
-import { getVoidLogger } from '@backstage/backend-common';
+import {
+  DatabaseManager,
+  getVoidLogger,
+  PluginDatabaseManager,
+} from '@backstage/backend-common';
 import { MockConfigApi } from '@backstage/test-utils';
 import express from 'express';
 import request from 'supertest';
 import { createRouter } from './router';
 import { CatalogClient } from '@backstage/catalog-client';
+import { ConfigReader } from '@backstage/config';
+
+function createDatabase(): PluginDatabaseManager {
+  return DatabaseManager.fromConfig(
+    new ConfigReader({
+      backend: {
+        database: {
+          client: 'better-sqlite3',
+          connection: ':memory:',
+        },
+      },
+    }),
+  ).forPlugin('health-check');
+}
 
 describe('createRouter', () => {
   let app: express.Express;
@@ -17,6 +35,7 @@ describe('createRouter', () => {
 
     const router = await createRouter({
       logger: getVoidLogger(),
+      databaseManager: createDatabase(),
       config: new MockConfigApi({}),
       catalogClient: new CatalogClient({ discoveryApi }),
     });
